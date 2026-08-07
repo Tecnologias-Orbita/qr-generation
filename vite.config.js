5e51a7b41c0cc1cc856eb3ec22511d6bb9827472
Lazaro <lachy200408@gmail.com>
2026-08-06 21:43:01 -0500

    mod: Minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 /** @type {import('vite').UserConfig} */
-export default {
-  plugins: [tailwindcss()],
-  input: {
-    index: resolve(import.meta.dirname, "index.html"),
-    decode: resolve(import.meta.dirname, "decode/index.html"),
-    transfer: resolve(import.meta.dirname, "transfermovil-transfer/index.html"),
-  },
-};
+export default defineConfig(() => {
+  return {
+    //@eslint-disable-next-line
+    base: process.env.NODE_ENV === "production" ? "/qr-generation/" : "/",
+    plugins: [tailwindcss()],
+    input: {
+      index: resolve(import.meta.dirname, "index.html"),
+      decode: resolve(import.meta.dirname, "decode/index.html"),
+      transfer: resolve(
+        import.meta.dirname,
+        "transfermovil-transfer/index.html",
+      ),
+    },
+  };
+});
